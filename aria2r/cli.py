7923af7f03b6c79e1b993a44aef426faf080b9d0
Darrick Herwehe <darrick@exitcodeone.com>
2021-03-14 13:19:42 -0400

    improve stdout printing
    changed all bare `print` statements to use a logger with correct levels, and added a `verbose` and `quiet` option to the command line args.

--- aria2r/cli.py
+++ aria2r/cli.py
@@ -6,13 +6,28 @@ Given an INPUT_FILE in the same format as aria2c input files, aria2r will
 use the jsonrpc api to add the downloads (with options) to a running instance
 of aria2c.
 """
+import logging
+import sys
 from pathlib import Path
-from pprint import pprint
+from pprint import pformat
 
 import configargparse
 from xdg import XDG_CONFIG_HOME
 
 from aria2r import api
+
+
+logging.basicConfig(
+	stream=sys.stdout, format="%(message)s", level=logging.INFO
+)
+log = logging.getLogger(__name__)
+
+
+def set_logging(args):
+	if args.verbose:
+		log.setLevel(logging.DEBUG)
+	elif args.quiet:
+		log.setLevel(logging.WARN)
 
 
 def parse_aria2_options(aria2_args):
@@ -45,14 +60,29 @@ def main():
 	p.add("--host")
 	p.add("--port")
 	p.add("--rpc-secret", default=None, help="secret text.")
+	p.add_argument(
+		"-v",
+		"--verbose",
+		action="store_true",
+		default=False,
+		help="a boolean flag.",
+	)
+	p.add_argument(
+		"-q",
+		"--quiet",
+		action="store_true",
+		default=False,
+		help="a boolean flag.",
+	)
 	config, extra_arguments = p.parse_known_args()
 
+	set_logging(config)
 	aria2_options = parse_aria2_options(extra_arguments)
 	with open(config.input_file) as inputfile:
 		downloads = api.parse(inputfile.read())
 	downloads = api.add_command_line_options(downloads, aria2_options)
-	pprint(config)
-	pprint(downloads)
+	log.debug(config)
+	log.info(pformat(downloads))
 
 
 if __name__ == "__main__":
